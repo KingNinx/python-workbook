e53e77d95a6ed49aa88cfcb6f976fe9a02005962
KingNin <142370610+KingNinx@users.noreply.github.com>
2023-09-29 07:01:54 +0800

    revision

--- makingChange.py
+++ makingChange.py
@@ -4,5 +4,29 @@ cpQuarter = 25
 cpDime = 10
 cpNickel = 5
 
-cents = input("How many cents?: ")
+cents = int(input("How many cents?: "))
+
+toonies = cents // cpToonie
+cents = cents % cpToonie
+
+loonies = cents // cpLoonie
+cents = cents % cpLoonie
+
+quarters = cents // cpQuarter
+cents = cents % cpQuarter
+
+dimes = cents // cpDime
+cents = cents % cpDime
+
+nickels = cents // cpNickel
+cents = cents % cpNickel
+
+pennies = cents
+
+print("Toonies:", toonies)
+print("Loonies:", loonies)
+print("Quarters:", quarters)
+print("Dimes:", dimes)
+print("Nickels:", nickels)
+print("Pennies:", pennies)
 
